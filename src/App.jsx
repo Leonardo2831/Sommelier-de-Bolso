312156c85930979430f57dbb333724d00b893d23
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-04 20:07:23 -0300

    Learned init react

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-    
+import React from "react";
+
+export default function App(){
+    const [count, setCount] = React.useState(0);
+
+    return (
+        <button 
+            onClick={() => {setCount(count + 1)}} 
+            className="px-4 py-1 bg-blue-900 text-blue-200">Clique aqui! {count}
+        </button>
+    );
+}
