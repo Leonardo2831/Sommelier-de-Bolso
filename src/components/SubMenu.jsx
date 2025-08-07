cfa04bb43653bfac688a5f3a8b2e26f162cfc750
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-07 14:15:18 -0300

    new style in menu

--- src/components/SubMenu.jsx
+++ src/components/SubMenu.jsx
@@ -1,12 +1,15 @@
 // deve desestruturar, pois os valores são passados dentro de um objeto
 const SubMenu = ({options}) => {
     return (
-        <ul>           
+        <ul className="absolute top-full -left-full 
+        flex flex-col items-center gap-3 w-[200px]
+        p-3 rounded-lg
+        bg-beige dark:bg-gray">           
             {options.map((option, index) => {  
                 const {text, link, element} = option;
 
                 return (
-                    <li key={index}>
+                    <li key={index} className='flex items-center justify-center'>
                         {
                             element ? element : link && text ? <a href={link}>{text}</a> : text
                         }
