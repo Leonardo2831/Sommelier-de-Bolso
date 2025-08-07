6beb78e165aa929a609ff9b6b9db27fa959595bc
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-06 21:54:29 -0300

    new path to files

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 const Logo = () => {
-    // colocando as imagens na pasta public consigo utilizar com o link seguido pelo arquivo.
-    const urlLogo = '../../public/icons/logo.png';
+    const urlLogo = '/public/icons/logo.png';
     return (
         <figure>
             <img src={urlLogo} alt="Sommelier de Bolso" />
