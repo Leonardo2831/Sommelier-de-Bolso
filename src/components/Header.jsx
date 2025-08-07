e1ae441949f761b9455de1b840e87eeb7fd5fcd3
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-07 13:06:40 -0300

    initial style to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import LinksNav from './header/LinksNav.jsx';
 import Logo from './Logo.jsx';
 
 const Header = () => {
     return ( 
-        <header>
+        <header className='flex justify-between items-center text-maple dark:text-gold'>
             <nav>
                 <LinksNav/>
             </nav>
