91c662ed0f7122067110f69b1fb3e6fb93dcd032
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-07 13:06:29 -0300

    remove stricted mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,6 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import '/src/styles/style.css';
 import App from "./App";
 
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <App></App>
-    </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App></App>);
