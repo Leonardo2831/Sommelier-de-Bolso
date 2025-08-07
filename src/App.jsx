f2d900fd5e3c9e183c5058a4c92104241ebc70c0
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-07 13:06:19 -0300

    remove button in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import React from "react";
 import AppProvider from './contexts/AppProvider.jsx'
-import useAppContext from "./hooks/useAppContext.jsx";
 
 import Header from './components/Header.jsx';
-import ButtonToggleMode from "./components/header/ButtonToggleMode.jsx";
 
 export default function App(){
     return (
         <AppProvider>
             <Header/>
-            <ButtonToggleMode/>
         </AppProvider>
     );
 }
