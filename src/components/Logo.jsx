21469f2ee60377069c90602e2c806677e016f8b7
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-07 14:14:13 -0300

    relative to subMenu

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,18 @@
+import useAppContext from "../hooks/useAppContext";
+
 import ButtonToggleMode from "./header/ButtonToggleMode";
 import SubMenu from "./SubMenu";
 
 const Logo = () => {
     // não usar public, pois a pasta public é ignorada pelo browser
     const urlLogo = '/icons/logo.png';
+    const altLogo = 'Sommelier de Bolso';
+
+    const {openMenu} = useAppContext();
 
     return (
-        <figure className='max-w-[100px] p-[10px] cursor-pointer'>
-            <img className='w-full' src={urlLogo} alt="Sommelier de Bolso" />
+        <figure onClick={openMenu} className='relative max-w-[100px] p-[10px] cursor-pointer'>
+            <img className='w-full' src={urlLogo} alt={altLogo} />
             <SubMenu options={
                 [
                     {text: 'Configurações'},
