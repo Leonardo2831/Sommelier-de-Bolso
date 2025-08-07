e42960b703e5cf3b543855e724a3b53ec13a3b5e
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-07 14:38:08 -0300

    function openMenu

--- src/contexts/AppProvider.jsx
+++ src/contexts/AppProvider.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import AppContext from './AppContext.jsx';
 
 const AppProvider = ({ children }) => {
+    // mode
     const [mode, setMode] = React.useState(() => {
         return localStorage.mode || '';
     });
 
+    // open sub-menu
+    function openMenu({currentTarget}){
+        if(currentTarget.children.length){
+            currentTarget.children[currentTarget.children.length - 1].classList.toggle('hidden');
+            currentTarget.children[currentTarget.children.length - 1].classList.toggle('flex');
+        }
+    }
+
     return (
-        <AppContext.Provider value={{ mode , setMode }}>
+        <AppContext.Provider value={{ mode , setMode, openMenu }}>
             {children}
         </AppContext.Provider>
     )
