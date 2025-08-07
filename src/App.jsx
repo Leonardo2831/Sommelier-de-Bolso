d38f8b5445f3ff24d75df999ef52fbe7674c19ad
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-07 11:12:24 -0300

    put context in items on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
 import React from "react";
+import AppProvider from './contexts/AppProvider.jsx'
+import useAppContext from "./hooks/useAppContext.jsx";
+
 import Header from './components/Header.jsx';
+import ButtonToggleMode from "./components/header/ButtonToggleMode.jsx";
 
 export default function App(){
-    const [mode, setMode] = React.useState(() => {
-        return localStorage.mode || '';
-    });
-
-    React.useEffect(() => {
-        localStorage.mode = mode;
-
-        const html = document.documentElement;
-        html.classList.toggle('dark');
-    }, [mode]);
-
-    const toggleMode = () => {
-        setMode(mode === 'dark' ? '' : 'dark');
-    }
-
     return (
-        // <> necessário, pois o componente retorna teóricamente 1 item apenas, logo é necessário retornar o elemento pai.
-        <>
-            <Header></Header>
-            <button 
-                onClick={toggleMode} 
-                className="px-4 py-1 bg-gold text-white cursor-pointer dark:bg-maple dark:text-beige">Clique aqui! modo: {mode || 'light'}
-            </button>
-        </>
+        <AppProvider>
+            <Header/>
+            <ButtonToggleMode/>
+        </AppProvider>
     );
 }
