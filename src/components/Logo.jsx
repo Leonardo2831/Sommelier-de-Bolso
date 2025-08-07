0f30c488b39aa30297c8154a07b29614aa9b82be
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-07 13:08:21 -0300

    initial style to submenu

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import ButtonToggleMode from "./header/ButtonToggleMode";
+import SubMenu from "./SubMenu";
 
 const Logo = () => {
     // não usar public, pois a pasta public é ignorada pelo browser
     const urlLogo = '/icons/logo.png';
+
     return (
-        <figure>
-            <img src={urlLogo} alt="Sommelier de Bolso" />
+        <figure className='max-w-[100px] p-[10px] cursor-pointer'>
+            <img className='w-full' src={urlLogo} alt="Sommelier de Bolso" />
+            <SubMenu options={
+                [
+                    {text: 'Configurações'},
+                    {element: <ButtonToggleMode/>}
+                ]
+            } />
         </figure>
     )
 }
