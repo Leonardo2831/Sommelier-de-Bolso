9deec7aac7cdfadf8ea7e06fbb8a4efe40f41a41
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-10 21:44:46 -0300

    design para passar dia dos pai

--- src/components/header/LinksNav.jsx
+++ src/components/header/LinksNav.jsx
@@ -13,7 +13,7 @@ const LinksNav = () => {
     }, [open]);
 
     return (
-        <ul className='flex flex-wrap items-center gap-5 *:p-2 *:cursor-pointer'>
+        <ul className='flex flex-wrap items-center gap-5 *:p-2 *:cursor-pointer *:hover:text-red *:transition-colors *:duration-200'>
             <li>
                 <a href="/index.html">Home</a>
             </li>
