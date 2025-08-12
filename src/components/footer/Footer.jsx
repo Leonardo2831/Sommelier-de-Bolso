40af7c87cb8b6eca7bec1617767d31499391476d
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-12 15:41:05 -0300

    initial footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Logo from '../Logo';
 
 const Footer = () => {
     return (
-        <footer>
-            
+        <footer className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+            <Logo visibleMenu={false}/>
+            <ul>
+                <li>
+                    <a href="#">Terroir</a>
+                    </li>
+                <li>
+                    <a href="#">Vinhos</a>
+                    </li>
+                <li>
+                    <a href="#">Barricas</a>
+                    </li>
+                <li>
+                    <a href="#">Aromas</a>
+                </li>
+            </ul>
         </footer>
     )
 }
