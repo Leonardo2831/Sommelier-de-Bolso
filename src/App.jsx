fcd26ca15ab9365460ecca37313b5c5adaa49fab
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-12 15:26:46 -0300

    new import

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import AppProvider from './contexts/AppProvider.jsx';
 import { Analytics } from "@vercel/analytics/react"
 
-import Header from './components/Header.jsx';
+import Header from './components/header/Header.jsx';
 import Main from './components/home/Main.jsx';
+import Footer from './components/footer/Footer.jsx';
 
 export default function App(){
     return (
         <AppProvider>
             <Header/>
             <Main/>
+            <Footer/>
             <Analytics/>
         </AppProvider>
     );
