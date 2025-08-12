94aca02c47d3e47c1899353bfc8a4fda99a79e96
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-12 00:46:09 -0300

    initial create main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import AppProvider from './contexts/AppProvider.jsx'
 
 import Header from './components/Header.jsx';
+import InitSection from './components/home/InitSection.jsx';
 
 export default function App(){
     return (
         <AppProvider>
             <Header/>
+            <main>
+                <InitSection/>
+            </main>
         </AppProvider>
     );
 }
