c7f442823ac1467a277841157f854e292abaa7a2
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-12 14:04:16 -0300

    new analytics of vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import AppProvider from './contexts/AppProvider.jsx'
+import AppProvider from './contexts/AppProvider.jsx';
+import { Analytics } from "@vercel/analytics/react"
 
 import Header from './components/Header.jsx';
-import InitSection from './components/home/InitSection.jsx';
+import Main from './components/home/Main.jsx';
 
 export default function App(){
     return (
         <AppProvider>
             <Header/>
-            <main>
-                <InitSection/>
-            </main>
+            <Main/>
+            <Analytics/>
         </AppProvider>
     );
 }
---
+++ src/components/home/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InitSection from './InitSection';
+
+const Main = () => {
+    return (
+        <main>
+            <InitSection/>
+        </main>
+    )
+}
+
+export default Main
