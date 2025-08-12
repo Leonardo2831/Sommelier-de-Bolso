4fd033849b399af197fae8743948c990e36fd51e
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-12 13:27:15 -0300

    new name images and remove one image

--- src/components/home/InitSection.jsx
+++ src/components/home/InitSection.jsx
@@ -38,10 +38,10 @@ const InitSection = () => {
     return (
         <section 
             className='flex items-center justify-center gap-[30px] p-[30px] 
-            max-sm:flex-col max-sm:flex-wrap max-sm:py-5 max-sm:px-3'
+            max-sm:flex-col max-sm:flex-wrap max-sm:py-5 max-sm:px-5'
         >
             <article className='flex-1 flex items-center justify-center basis-[200px]'>
-                <h1 className='text-5xl text-maple dark:text-gold max-sm:4xl [@media(max-width:480px)]:text-3xl'>
+                <h1 className='text-5xl text-maple dark:text-gold max-sm:text-4xl'>
                     Seja um 
                     <br/>
                     <span className='font-semibold text-red dark:text-beige'>Sommelier </span> 
@@ -56,11 +56,11 @@ const InitSection = () => {
                 className='relative flex-1 flex items-center justify-center sm:basis-[300px] sm:justify-start'
             >
                 <img 
-                    className='-rotate-[18deg] min-w-[420px] max-sm:min-w-[300px] max-sm:-translate-x-[40px]' 
+                    className='-rotate-[18deg] max-w-[420px] w-full max-sm:max-w-[300px] max-sm:-translate-x-[40px]' 
                     src="../../../public/images/details-background.png" 
                     alt="Detalhe de fundo" 
                 />
-                <img className='absolute top-1/2 left-1/2 -translate-1/2 w-full max-w-[300px] max-sm:max-w-[250px] z-10' src={initImageSrc} alt={initImageAlt}/>
+                <img className='absolute top-1/2 left-1/2 -translate-1/2 w-[250px] h-[400px] object-contain max-sm:w-[250px] z-10' src={initImageSrc} alt={initImageAlt}/>
             </figure>
         </section>
     )
