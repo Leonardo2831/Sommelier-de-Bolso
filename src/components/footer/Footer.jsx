62138ecc3601048347eb89d43bc7a6b5c12db77a
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-12 22:25:29 -0300

    new code to create list links in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
 import Logo from '../Logo';
 import ContactInfo from './ContactInfo';
 
-const Footer = () => {
-    const listFooter = [
-        {text: 'Terroir', link: '#'},
-        {text: 'Barricas', link: '#'},
-        {text: 'Aromas', link: '#'},
-        {text: 'Uvas', link: '#'},
-        {text: 'IA', link: '#'},
-    ];
-
+const Footer = ({ listFooter }) => {
     return (
-        <footer className='px-10 py-[30px] grid items-center justify-items-center grid-cols-1 gap-5 text-maple dark:text-gold sm:grid-cols-2 lg:grid-cols-3'>
+        <footer 
+            className='px-10 py-[30px] 
+            grid items-center justify-items-center grid-cols-1 gap-10 sm:gap-5 
+            md:grid-cols-2 lg:grid-cols-3'
+        >
             <a href="/index.html">
                 <Logo visibleMenu={false}/>
             </a>
-            <ul className='list-disc text-2xl font-medium *:mt-3 *:first:mt-0'>
+            <ul className='list-disc *:mt-3 *:first:mt-0'>
                 {
                     listFooter.map((listItem, index) => {
                         const {text, link} = listItem;
 
                         return (
-                            <li key={index}>
+                            <li key={index} 
+                                className='text-2xl font-medium 
+                                text-maple dark:text-gold hover:text-red dark:hover:text-beige'
+                            >
                                 <a href={link}>{text}</a>
                             </li>
                         )
@@ -35,14 +33,17 @@ const Footer = () => {
                     {src: '/icons/Instagram.png', alt: 'Instagram', link: '#'},
                     {src: '/icons/LinkedIn.png', alt: 'LinkedIn', link: '#'},
                     {element: (
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <svg className='w-7 h-7' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                             <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                             <polyline points="22,6 12,13 2,6"></polyline>
                         </svg>
                     ), alt: 'Email', link: '#'}
                 ]}
             />
-            <div className='col-span-3 p-5 text-center font-sans text-sm font-medium'>
+            <div 
+                className='p-3 text-center font-sans text-sm font-medium 
+                text-gray dark:text-beige md:col-span-2 lg:col-span-3 lg:p-0 lg:pt-5'
+            >
                 <p>As imagens de vinhos exibidas neste site são propriedade das vinícolas originais e são utilizadas apenas para fins ilustrativos.</p>
             </div>
         </footer>
