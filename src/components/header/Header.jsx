f1c264780c184a3257b5ca6914a61e299aee86d7
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-12 21:45:59 -0300

    new style in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,7 @@ import Logo from '../Logo.jsx';
 
 const Header = () => {
     return ( 
-        <header 
-            className='flex justify-between items-center gap-3 px-10 py-[30px] 
-            font-medium text-maple dark:text-gold max-sm:px-[30px]'
-        >
+        <header className='flex justify-between items-center gap-3 px-10 py-[30px] max-sm:px-[30px]'>
             <nav>
                 <LinksNav/>
             </nav>
