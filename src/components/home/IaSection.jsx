ab4d4685a8eacca9f9614dd4b3417bdfd22897e0
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-13 11:35:54 -0300

    new components added

--- src/components/home/IaSection.jsx
+++ src/components/home/IaSection.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
+import FormIA from './FormIA';
 
 const IaSection = () => {
     return (
-        <section>
-            <h1 className='text-center font-medium text-5xl text-maple dark:text-gold'>
+        <section className='flex flex-col items-center'>
+            <h1 className='text-center font-medium text-5xl text-maple dark:text-gold mb-5'>
                 Não encontrou<span className='text-gold dark:text-red'>?</span>
             </h1>
-            <h2 className='text-center font-light text-4xl text-gray dark:text-cream'>
+            <h2 className='text-center font-light text-4xl text-gray dark:text-cream mb-[30px]'>
                 Consulte nossa <span className='font-regular text-red dark:text-gold'>IA</span> sommleier
             </h2>
-            <form 
-                className='max-w-[600px] p-[60px]'
-                action="#" 
-                onSubmit={(event) => event.preventDefault()}
-            >
-            </form>
+            <FormIA/>
         </section>
     )
 }
