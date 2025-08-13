afd41480737123f0d1164f10c2d01f85a51656b2
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-12 22:39:44 -0300

    initial html request ia

--- src/components/home/IaSection.jsx
+++ src/components/home/IaSection.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const IaSection = () => {
     return (
         <section>
-
+            <h1 className='text-center font-medium text-5xl text-maple dark:text-gold'>
+                Não encontrou<span className='text-gold dark:text-red'>?</span>
+            </h1>
+            <h2 className='text-center font-light text-4xl text-gray dark:text-cream'>
+                Consulte nossa <span className='font-regular text-red dark:text-gold'>IA</span> sommleier
+            </h2>
+            <form 
+                className='max-w-[600px] p-[60px]'
+                action="#" 
+                onSubmit={(event) => event.preventDefault()}
+            >
+            </form>
         </section>
     )
 }
