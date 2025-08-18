afdfddef72ed200b59d8c9cbf088421d6e3c2d84
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-17 21:05:28 -0300

    new infos wine

--- public/data/infosWines.js
+++ public/data/infosWines.js
@@ -8,15 +8,15 @@ const wineLiqueur = wines.filter((wine) => wine.type === 'liqueur');
 
 export const infosSparkling = {
     title: 'Espumante',
-    text: '',
-    listCharacteristics: [],
+    text: 'O espumante é um vinho no qual em sua segunda fermentação, ocorre um processo de aprisionamento do dióxido de carbono liberado. Esse aprisionamento da origem à sua perlage e tras aromas e sabores únicos para cada produção.',
+    listCharacteristics: ['Os aromas presentes vão de frutas cítricas até aromas mais complexos.', 'Elevada e crocante, conferindo grande frescor.', 'Apresenta uma vasta gama por sua variedade, como seco e suave, podendo ser uma boa opção em várias ocasiões.'],
     wineSelected: wineSparkling[Math.floor(Math.random() * wineSparkling.length)],
 };
 
 export const infosWhite = {
     title: 'Branco',
-    text: '',
-    listCharacteristics: [],
+    text: 'O vinho branco é porduzido majoritariamente a partir de fermentação de mosto sem contato com as cascas. Ele revela um paladar que pode variar de leve e crítico ao escorpado e amanteigado pelo fato de ter pouco tanino pela falta de contato da casca.',
+    listCharacteristics: ['Seu corpo varia de leve a untuoso e incorpado.', 'Com uma acidez mais perceptível e refrescante, trazendo leveza ao paladar.', 'Um amplo espectro de aromas, trazendo com tons frutados e florais, como mel, flores, frutas cítricas, variando de cada vinho.'],
     wineSelected: wineWhite[Math.floor(Math.random() * wineWhite.length)],
 };
 
@@ -29,8 +29,8 @@ export const infosRose = {
 
 export const infosRed = {
     title: 'Tinto',
-    text: '',
-    listCharacteristics: [],
+    text: 'O vinho tinto é a complexidade do mundo do vinho, vindo do contato do mosto da uva durante sua fermentação. Esse processo, a maceração, lhe traz taninos, cor e uma grande gama de aromas, seu resultado pode ser um vinho jovem para algo até mesmo um exemplar potente e robusto.',
+    listCharacteristics: ['Sua acidez geralmente sendo menos pontiaguda perante os vinhos brancos, é algo vital e único do vinho tinto', 'Aromas tendem a ser de frutas vermelhas e pretas (como morango e ameixa) e também especíarias. Esses aromas podem variar com nas ena maturação.', 'Apresenta um corpo de enorme variação, podendo ser leve e elegante, até mesmo encorpado e potente, influenciado principalmente pelas uvas usadas, teor alcóolico e concentração de taninos.'],
     wineSelected: wineRed[Math.floor(Math.random() * wineRed.length)],
 };
 
