f2d88607eefa0600e9e430f588fa51470a3b9bda
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-21 11:18:09 -0300

    new title and description in home

--- src/components/pages/home/Main.jsx
+++ src/components/pages/home/Main.jsx
@@ -4,9 +4,11 @@ import IaSection from './IaSection';
 import InitSection from './InitSection';
 
 const Main = () => {
+    const description = 'Veja sobre vinhos de forma simples e interativa com o Sommelier de Bolso e seja seu próprio sommelier. Explore efeitos das barricas, tipos de uva, aromas, terroir e conte com a ajuda de uma IA para entender melhor cada detalhe do mundo do vinho.';
+
     return (
         <main>
-            <Head title="Sommelier de bolso | | Home" />
+            <Head titleText="Sommelier de bolso" description={description} />
             <InitSection/>
             <ChoiceTypeWine/>
             <IaSection/>
