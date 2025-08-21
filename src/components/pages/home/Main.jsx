c97d72a0aff7a9f8fc359d1213930d584861fe65
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-20 22:06:45 -0300

    Added component head in main

--- src/components/pages/home/Main.jsx
+++ src/components/pages/home/Main.jsx
@@ -1,3 +1,4 @@
+import Head from '../../Head';
 import ChoiceTypeWine from './ChoiceTypeWine';
 import IaSection from './IaSection';
 import InitSection from './InitSection';
@@ -5,6 +6,7 @@ import InitSection from './InitSection';
 const Main = () => {
     return (
         <main>
+            <Head title="Sommelier de bolso | | Home" />
             <InitSection/>
             <ChoiceTypeWine/>
             <IaSection/>
