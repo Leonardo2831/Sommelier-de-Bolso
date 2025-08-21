b110162f622ac7c9095feb529d9cec92e34f615f
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-21 17:36:01 -0300

    init oakBarrel main

--- src/components/pages/oakBarrel/MainOakBarrel.jsx
+++ src/components/pages/oakBarrel/MainOakBarrel.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import Head from '../../Head';
+import InitPages from '../../initPages';
 
 const MainOakBarrel = () => {
     return (
         <main>
             <Head titleText='Barricas | Sommelier de Bolso' />
+            <InitPages
+                title='Barricas'
+                optionImage={true} 
+                srcImage={'/public/images/background-barrel.jpg'} 
+                altImage={'Barricas'}
+            />
         </main>
     )
 }
