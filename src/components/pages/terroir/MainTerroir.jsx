687e97770d32a0ecd8647bc28b32e3f48d858671
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-21 13:16:41 -0300

    init section terroir

--- src/components/pages/terroir/MainTerroir.jsx
+++ src/components/pages/terroir/MainTerroir.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import Head from '../../Head';
+import InitPages from '../../initPages';
 
 const MainTerroir = () => {
     return (
         <main>
             <Head titleText='Terroir | Sommelier de Bolso' />
+            <InitPages 
+                title='Terroir'
+                optionVideo={true} 
+                srcVideo={'/video/terroir-movement.mp4'} 
+                imageOption={'/video/background-terroir.jpg'} 
+                typeVideo='video/mp4' 
+            />
         </main>
     )
 }
