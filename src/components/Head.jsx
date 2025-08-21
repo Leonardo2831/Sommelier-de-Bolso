195641a7788e95d03d7dac60e61a2422ea31c71d
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-21 11:17:57 -0300

    new code to description

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Head = (props) => {
     React.useEffect(() => {
-        document.title = props.titleText;
-        document.querySelector('[meta[name="description"]]')?.setAttribute('content', props.description);
+        document.title = props.titleText ? props.titleText : 'Sommelier de Bolso';
+
+        if (props.description) {
+            const metaDescription = document.querySelector('meta[name="description"]');
+            
+            if (metaDescription) {
+                metaDescription.setAttribute('content', props.description);
+            } else {
+                const meta = document.createElement('meta');
+
+                meta.name = 'description';
+                meta.content = props.description;
+                document.head.appendChild(meta);
+            }
+        }
+
     }, [props]);
 
     return <></>;
