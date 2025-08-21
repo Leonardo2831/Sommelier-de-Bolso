f08ff55fee9d3a5c47861abcd79f1988bc9bfc0a
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-21 17:35:19 -0300

    new style init image

--- src/components/InitPages.jsx
+++ src/components/InitPages.jsx
@@ -7,7 +7,7 @@ const InitPages = (props) => {
             {props.optionVideo && <Video srcVideo={props.srcVideo} imageOption={props.imageOption} typeVideo={props.typeVideo} />}
             {props.optionImage && <ImageBackground srcImage={props.srcImage} altImage={props.altImage} />}
             <div className='bg-shadow-background absolute inset-0 flex items-center justify-center'>
-                <h1 className='font-bold text-5xl text-cream sm:text-8xl'>{props.title}</h1>
+                <h1 className='font-bold text-5xl text-cream sm:text-8xl lg:text-[148px]'>{props.title}</h1>
             </div>
         </section>
     )
