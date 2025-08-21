bf024449ad4211c866a18f0718d1ba8b79581c8f
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-21 13:16:25 -0300

    code to image

---
+++ src/components/ImageBackground.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImageBackground = ({ srcImage, altImage }) => {
+    return (
+        <img src={srcImage} alt={altImage} />
+    )
+}
+
+export default ImageBackground;
