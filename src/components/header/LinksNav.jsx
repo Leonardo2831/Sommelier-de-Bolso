5a3e10e9c3c68555ce838a04b1093ab310ae27da
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-21 17:35:53 -0300

    new code links nav

--- src/components/header/LinksNav.jsx
+++ src/components/header/LinksNav.jsx
@@ -1,46 +1,38 @@
-import React from 'react';
-import initClickOutside from '../../javascript/clickOutside.js';
+import { NavLink } from 'react-router-dom';
 
-import SubMenu from '../SubMenu.jsx';
-import { Link } from 'react-router-dom';
-
-const LinksNav = () => {
-    const [open, setOpen] = React.useState(false);
-
-    const menuRef = React.useRef(null);
-
-    React.useEffect(() => {
-        initClickOutside(open, setOpen, menuRef);
-    }, [open]);
-
+const LinksNav = ({ options }) => {
     return (
         <ul 
-            className='flex flex-wrap items-center gap-3 
+            className='hidden flex-wrap items-center gap-3 
             *:p-2 *:text-2xl *:font-medium *:cursor-pointer 
             *:text-maple *:hover:text-red dark:*:text-gold dark:*:hover:text-beige
-            *:transition-colors *:duration-150 sm:*:text-[32px] md:text-[32px]'>
-            <li>
-                <Link to='/'>Home</Link>
-            </li>
-            <li onClick={() => {setOpen(!open)}} data-button-menu className='relative'>
-                <p>Vinhos</p>
-                <SubMenu 
-                    ref={menuRef}
-                    transform="left-valueLinks"
-                    options={
-                        [
-                            { text: 'Barricas', link: 'Barricas'}, 
-                            { text: 'Aromas', link: 'Aromas'},
-                            { text: 'uvas', link: 'Uvas'},
-                        ]
-                    } 
-                />
-            </li>
-            <li>
-                <Link to='Terroir'>Terroir</Link>
-            </li>
+            *:transition-colors *:duration-150 sm:*:text-[32px] md:text-[32px] sm:flex'>
+            {options.map((option, index) => {
+                return (
+                    <li key={index}>
+                        <NavLink to={option.link}>{option.text}</NavLink>
+                    </li>
+                )
+            })}
         </ul>
     )
-}
+};
 
 export default LinksNav;
+
+
+// <li onClick={() => {setOpen(!open)}} data-button-menu className='relative'>
+//                 <SubMenu 
+//                     ref={menuRef}
+//                     transform="left-valueLinks"
+//                     options={
+//                         [
+//                             { text: 'Home', link: '/'},
+//                             { text: 'Barricas', link: 'Barricas'}, 
+//                             { text: 'Aromas', link: 'Aromas'},
+//                             { text: 'Uvas', link: 'Uvas'},
+//                             { text: 'Terroir', link: 'Terroir'},
+//                         ]
+//                     } 
+//                 />
+//             </li>
