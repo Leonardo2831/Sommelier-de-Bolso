6eca67c4e139a3ca958afc749a832892cb6df32a
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-21 15:48:42 -0300

    component initPage

--- src/components/InitPages.jsx
+++ src/components/InitPages.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-
 import Video from './VideoBackground';
 import ImageBackground from './ImageBackground';
 
-const InitPages = () => {
+const InitPages = (props) => {
     return (
         <section className='relative'>
             {props.optionVideo && <Video srcVideo={props.srcVideo} imageOption={props.imageOption} typeVideo={props.typeVideo} />}
             {props.optionImage && <ImageBackground srcImage={props.srcImage} altImage={props.altImage} />}
             <div className='bg-shadow-background absolute inset-0 flex items-center justify-center'>
-                <h1 className='font-bold text-8xl'>{props.title}</h1>
+                <h1 className='font-bold text-5xl text-cream sm:text-8xl'>{props.title}</h1>
             </div>
         </section>
     )
