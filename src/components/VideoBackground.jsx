ed07f691976e558aa2246726f5d2059a575fca07
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-21 13:17:00 -0300

    component video background

---
+++ src/components/VideoBackground.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const VideoBackground = ({ optionImage, srcVideo, typeVideo }) => {
+    const refVideo = React.useRef(null);
+
+    React.useEffect(() => {
+        if(refVideo){
+            refVideo.current.playbackRate = 0.5;
+        }
+    }, []);
+
+    return (
+        <video ref={refVideo} 
+            className='w-full' 
+            poster={optionImage} 
+            loop muted autoPlay
+        >
+            <source src={srcVideo} type={typeVideo} />
+        </video>
+    )
+}
+
+export default VideoBackground;
