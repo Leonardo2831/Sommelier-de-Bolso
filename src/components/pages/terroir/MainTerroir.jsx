cf32ee86f49479ca7b05b92d6a9f979a5017400c
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-22 00:59:56 -0300

    added new components

--- src/components/pages/terroir/MainTerroir.jsx
+++ src/components/pages/terroir/MainTerroir.jsx
@@ -1,5 +1,9 @@
+import { Link } from 'react-router-dom';
+
 import Head from '../../Head';
 import InitPages from '../../initPages';
+import AboutTerroir from './AboutTerroir';
+import AffectedGrapes from './AffectedGrapes';
 
 const MainTerroir = () => {
     return (
@@ -12,6 +16,18 @@ const MainTerroir = () => {
                 imageOption={'/video/background-terroir.jpg'} 
                 typeVideo='video/mp4' 
             />
+            <AboutTerroir/>
+            <AffectedGrapes/>
+            <div className='my-5 flex justify-center items-center'>
+                <Link to="Uvas" 
+                    className='uppercase font-semibold font-sans 
+                    text-2xl px-9 py-3 bg-gold text-white
+                    max-w-[300px] w-full rounded-[10px] text-center'
+                >
+                    ver mais -&gt;
+                </Link>
+            </div>
+            
         </main>
     )
 }
