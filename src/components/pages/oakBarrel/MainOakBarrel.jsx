383a8379d81bf852c45f99172217a90744023c12
Leonardo Reis Ferraz <129678079+Leonardo2831@users.noreply.github.com>
2025-08-22 23:32:16 -0300

    Update MainOakBarrel import

--- src/components/pages/oakBarrel/MainOakBarrel.jsx
+++ src/components/pages/oakBarrel/MainOakBarrel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Head from '../../Head';
-import InitPages from '../../initPages';
+import InitPages from '../../InitPages';
 
 const MainOakBarrel = () => {
     return (
