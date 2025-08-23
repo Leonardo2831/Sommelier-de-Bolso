a4ccc988c06ca4c862d8eeb447e3d759ce001c92
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-23 00:30:12 -0300

    new variables text

--- src/components/pages/terroir/MainTerroir.jsx
+++ src/components/pages/terroir/MainTerroir.jsx
@@ -4,8 +4,22 @@ import Head from '../../Head';
 import InitPages from '../../initPages';
 import AboutTerroir from './AboutTerroir';
 import AffectedGrapes from './AffectedGrapes';
+import SectionListTerroir from './SectionListTerroir';
 
 const MainTerroir = () => {
+    const textClimate = 'Cria a condição necessária para o melhor amadurecimento da uva, definindo a “alma do vinho”.';
+    const listClimate = [
+        '<strong className="text-[22px] font-semibold">Macroclima: </strong><span>O clima geral de uma grande região.</span>',
+        '<strong className="text-[22px] font-semibold">Mesoclima: </strong><span>O clima específico de um vinhedo ou de uma encosta, influenciado diretamente pelo relevo.</span>',
+        '<strong className="text-[22px] font-semibold">Microclima: </strong><span>O clima imediato ao redor da videira e do cacho de uva, que pode ser manejado pela ação humana (poda, por exemplo).</span>',
+    ];
+        
+    const textRelief = 'O relevo, que engloba a topografia, altitude, inclinação e orientação das encostas, é um dos pilares que definem o terroir de um vinho. Ele age como um grande modulador do clima e interage diretamente com o solo, influenciando de maneira decisiva como a videira se desenvolve e, consequentemente, o estilo e a qualidade do vinho.';
+    const listRelief = [
+        'Quanto maior a altitude, maior a diferença de temperatura entre o dia e a noite. Dias quentes e ensolarados permitem o perfeito amadurecimento das uvas, enquanto as noites frias preservam a acidez natural da fruta.',
+        'Em altitudes mais elevadas, a radiação ultravioleta é mais intensa.',
+    ];
+
     return (
         <main>
             <Head titleText='Terroir | Sommelier de Bolso' />
@@ -27,7 +41,18 @@ const MainTerroir = () => {
                     ver mais -&gt;
                 </Link>
             </div>
-            
+            <SectionListTerroir 
+                title='Clima' 
+                text={textClimate} 
+                list={listClimate}
+            />
+            <SectionListTerroir 
+                title='Papel do relevo' 
+                text={textRelief} 
+                list={listRelief} 
+                srcImage='/images/relevo.jpg'
+                altImage='Relevo'
+            />
         </main>
     )
 }
