83ab144266d08027ecb0f2ba31bb2ae5fb9b5334
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-23 00:29:30 -0300

    style grape terroir

--- src/components/pages/terroir/GrapeTerroir.jsx
+++ src/components/pages/terroir/GrapeTerroir.jsx
@@ -1,11 +1,11 @@
 const GrapeTerroir = (props) => {
     return (
         <div className="flex flex-col gap-5 sm:flex-row">
-            <div className="flex-2 sm:min-w-[350px]">
+            <div className="flex-2 sm:min-w-[330px]">
                 <h4 className="mb-[15px] font-medium text-[30px] text-gray dark:text-cream sm:text-3xl">{props.nameGrape}</h4>
                 <p className="font-sans text-xl text-light-gray dark:text-beige">{props.textGrape}</p>
             </div>
-            <figure className="self-center max-w-[300px] min-w-[250px]">
+            <figure className="self-center max-w-[300px] min-w-[220px]">
                 <img className="w-full aspect-square rounded-lg" 
                     src={props.srcImage} alt={props.nameGrape} 
                 />
