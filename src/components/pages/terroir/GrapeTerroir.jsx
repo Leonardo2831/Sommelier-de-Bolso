d5c394d5295bd595758c79c1487130b5b8f6f0da
Leonardo2831 <leoferraz1818@gmail.com>
2025-08-27 13:29:38 -0300

    new style to vivid grape

--- src/components/pages/terroir/GrapeTerroir.jsx
+++ src/components/pages/terroir/GrapeTerroir.jsx
@@ -6,7 +6,7 @@ const GrapeTerroir = (props) => {
                 <p className="font-sans text-xl text-light-gray dark:text-beige">{props.textGrape}</p>
             </div>
             <figure className="self-center max-w-[300px] min-w-[220px]">
-                <img className="shadow w-full aspect-square rounded-lg" 
+                <img className="brightness-90 contrast-110 saturate-120 shadow w-full aspect-square rounded-lg" 
                     src={props.srcImage} alt={props.nameGrape} 
                 />
             </figure>
