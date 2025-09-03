48547098400ff498695bbed373726f427810ab1a
Leonardo2831 <leoferraz1818@gmail.com>
2025-09-03 20:12:27 -0300

    new grid style to grapes effected

--- src/components/pages/terroir/AffectedGrapes.jsx
+++ src/components/pages/terroir/AffectedGrapes.jsx
@@ -9,11 +9,11 @@ const AffectedGrapes = () => {
             <h3 className='mb-[30px] text-[32px] font-semibold text-maple dark:text-gold sm:text-[36px]'>
                 Uvas mais sensíveis às mudanças do terroir
             </h3>
-            <div className='grid gap-[50px]'>
-                <GrapeTerroir nameGrape='Riesling' textGrape={textRiesling} 
+            <div className='grid gap-[30px] sm:grid-cols-[minmax(200px,1fr)_minmax(200px,1fr)] sm:grid-rows-[auto_400px_300px_300px]'>
+                <GrapeTerroir gridClassText='sm:col-span-2' gridClassImage='sm:col-span-2' nameGrape='Riesling' textGrape={textRiesling} 
                     srcImage='https://upload.wikimedia.org/wikipedia/commons/c/c4/Grape_near_Sancerre.jpg' 
                 />
-                <GrapeTerroir nameGrape='Pinot Noir' textGrape={textPinotNoir} 
+                <GrapeTerroir gridClassText='sm:col-span-1 row-span-2 self-center' gridClassImage='sm:row-span-2' nameGrape='Pinot Noir' textGrape={textPinotNoir} 
                     srcImage='https://upload.wikimedia.org/wikipedia/commons/7/76/Riesling_grapes_leaves.jpg' 
                 />
             </div>
--- src/components/pages/terroir/GrapeTerroir.jsx
+++ src/components/pages/terroir/GrapeTerroir.jsx
@@ -1,16 +1,22 @@
 const GrapeTerroir = (props) => {
+    const classText = `${props.gridClassText || ''}`;
+    const classImage = `${props.gridClassImage || ''} object-cover brightness-90 contrast-110 saturate-120 shadow w-full h-full rounded-lg`;
+    
     return (
-        <div className="flex flex-col gap-5 sm:flex-row">
-            <div className="flex-2 sm:min-w-[330px]">
-                <h4 className="mb-[15px] font-medium text-[30px] text-gray dark:text-cream sm:text-3xl">{props.nameGrape}</h4>
-                <p className="font-sans text-xl text-light-gray dark:text-beige">{props.textGrape}</p>
+        <>
+            <div className={classText}>
+                <h4 className="mb-[15px] font-medium text-[30px] text-gray dark:text-cream sm:text-3xl">
+                    {props.nameGrape}
+                </h4>
+                <p className="font-sans text-xl text-light-gray dark:text-beige">
+                    {props.textGrape}
+                </p>
             </div>
-            <figure className="self-center max-w-[300px] min-w-[220px]">
-                <img className="brightness-90 contrast-110 saturate-120 shadow w-full aspect-square rounded-lg" 
-                    src={props.srcImage} alt={props.nameGrape} 
-                />
-            </figure>
-        </div>
+
+            <img className={classImage}
+                src={props.srcImage} alt={props.nameGrape} 
+            />
+        </>
     )
 }
 
