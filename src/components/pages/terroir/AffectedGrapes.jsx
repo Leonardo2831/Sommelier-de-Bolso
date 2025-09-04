d55088d45f20616f1b6922b5c7f3e62b475f889a
Leonardo2831 <leoferraz1818@gmail.com>
2025-09-03 23:41:58 -0300

    use base data grapes

--- src/components/pages/terroir/AffectedGrapes.jsx
+++ src/components/pages/terroir/AffectedGrapes.jsx
@@ -1,8 +1,7 @@
+import grapesInfo from '../../../../public/data/grapes.js';
 import GrapeTerroir from './GrapeTerroir';
 
 const AffectedGrapes = () => {
-    const textRiesling = 'Ela enfrenta enorme dificuldade para se adaptar fora de suas regiões de origem (principalmente Alemanha e Alsácia). Sua sensibilidade ao tipo de solo e clima é muito alta, não se adaptando a ficar com outras uvas. Além disso, ela não se beneficia do envelhecimento em madeira, o que dificulta ainda mais seu cultivo e vinificação em outras regiões. ';
-    const textPinotNoir = 'Para seu cultivo, é necessário solo e clima em perfeita sintonia, leveduras nativas e o cuidadoso uso do amadurecimento em madeira, são eles requisitos mínimos para o sucesso dessa exigente uva. Contudo, com todos os requisitos, a recompensa é extremamente compensadora, resultando em vinhos elegantes e de complexidade singurares.';
 
     return (
         <section className='p-10'>
@@ -10,11 +9,16 @@ const AffectedGrapes = () => {
                 Uvas mais sensíveis às mudanças do terroir
             </h3>
             <div className='grid gap-[30px] sm:grid-cols-[minmax(200px,1fr)_minmax(200px,1fr)] sm:grid-rows-[auto_400px_300px_300px]'>
-                <GrapeTerroir gridClassText='sm:col-span-2' gridClassImage='sm:col-span-2' nameGrape='Riesling' textGrape={textRiesling} 
-                    srcImage='https://upload.wikimedia.org/wikipedia/commons/c/c4/Grape_near_Sancerre.jpg' 
+                <GrapeTerroir 
+                    gridClassText='sm:col-span-2' gridClassImage='sm:col-span-2' 
+                    nameGrape={grapesInfo.riesling.nameGrape} 
+                    textGrape={grapesInfo.riesling.textInfoGrape} 
+                    srcImage={grapesInfo.riesling.urlImageGrape} 
                 />
-                <GrapeTerroir gridClassText='sm:col-span-1 row-span-2 self-center' gridClassImage='sm:row-span-2' nameGrape='Pinot Noir' textGrape={textPinotNoir} 
-                    srcImage='https://upload.wikimedia.org/wikipedia/commons/7/76/Riesling_grapes_leaves.jpg' 
+                <GrapeTerroir gridClassText='sm:col-span-1 row-span-2 self-center' gridClassImage='sm:row-span-2'               
+                    nameGrape={grapesInfo.pinot_noir.nameGrape} 
+                    textGrape={grapesInfo.pinot_noir.textInfoGrape} 
+                    srcImage={grapesInfo.pinot_noir.urlImageGrape} 
                 />
             </div>
         </section>
