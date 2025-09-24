459fe34ad3e8078b85604b9c5265fea1aed9fb0b
Leonardo2831 <leoferraz1818@gmail.com>
2025-09-23 22:28:44 -0300

    correct terroir main

--- src/components/pages/terroir/AffectedGrapes.jsx
+++ src/components/pages/terroir/AffectedGrapes.jsx
@@ -1,6 +1,23 @@
-import GrapeTerroir from './GrapeTerroir';
+import React from 'react';
+import GrapeTerroir from './GrapeTerroir'
+import AppContext from '../../../contexts/AppContext';
 
 const AffectedGrapes = () => {
+
+    const { allItems } = React.useContext(AppContext);
+    const [grapes, setGrapes] = React.useState([]);
+    
+    React.useEffect(() => {       
+        const affected = [];
+
+        allItems.forEach((item) => {
+            if(item.id == 'riesling' || item.id == 'pinot_noir'){
+                affected.push(item); 
+            }
+        });
+
+        setGrapes(affected);
+    }, [allItems]);
 
     return (
         <section className='p-10'>
@@ -8,17 +25,26 @@ const AffectedGrapes = () => {
                 Uvas mais sensíveis às mudanças do terroir
             </h3>
             <div className='grid gap-[30px] sm:grid-cols-[minmax(200px,1fr)_minmax(200px,1fr)] sm:grid-rows-[auto_400px_300px_300px]'>
-                <GrapeTerroir 
-                    gridClassText='sm:col-span-2' gridClassImage='sm:col-span-2' 
-                    nameGrape={grapesInfo.riesling.grapeName} 
-                    textGrape={grapesInfo.riesling.textInfoGrape} 
-                    srcImage={grapesInfo.riesling.urlImageGrape} 
-                />
-                <GrapeTerroir gridClassText='sm:col-span-1 row-span-2 self-center' gridClassImage='sm:row-span-2'               
-                    nameGrape={grapesInfo.pinot_noir.grapeName} 
-                    textGrape={grapesInfo.pinot_noir.textInfoGrape} 
-                    srcImage={grapesInfo.pinot_noir.urlImageGrape} 
-                />
+                <>
+                    {grapes[0] &&
+                        <GrapeTerroir 
+                            gridClassText='sm:col-span-2' 
+                            gridClassImage='sm:col-span-2' 
+                            nameGrape={grapes[0].grapeName} 
+                            textGrape={grapes[0].textInfoGrape} 
+                            srcImage={grapes[0].url} 
+                        />
+                    }     
+                    {grapes[1] && 
+                    <GrapeTerroir 
+                            gridClassText='sm:col-span-1 row-span-2 self-center'
+                            gridClassImage='sm:row-span-2'               
+                            nameGrape={grapes[1].grapeName} 
+                            textGrape={grapes[1].textInfoGrape} 
+                            srcImage={grapes[1].url} 
+                        />
+                    }
+                </>
             </div>
         </section>
     )
