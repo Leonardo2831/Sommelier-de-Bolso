d87c1323066019cbc0413283956dc0f20ee6ed97
Leonardo2831 <leoferraz1818@gmail.com>
2025-09-24 22:21:06 -0300

    new code to toolTip

--- src/components/pages/grapes/MosaicImage.jsx
+++ src/components/pages/grapes/MosaicImage.jsx
@@ -3,43 +3,50 @@ import { Link } from 'react-router-dom';
 
 const MosaicImage = ({item, classItem}) => {   
     const [hasHover, setHasHover] = React.useState(false);
-    const [mousePositionItem, setMousePositionItem] = React.useState({x: 0, y: 0, element: <></>});
-    const [mouseLeaveItem, setMouseLeaveItem] = React.useState(true);
+    const [mouseInfos, setMouseInfos] = React.useState({x: 0, y: 0, height: 0});
+    const [isHovering, setIsHovering] = React.useState(false);
+
+    const toolTipRef = React.useRef(null);
 
     React.useEffect(() => {
-        setHasHover(window.matchMedia("(hover: hover) and (pointer: fine)").matches)
+        setHasHover(!('ontouchstart' in window))
     }, []);
 
-    function handleMouseOver(){
-        const toolTip = mousePositionItem.element;
-        // console.log(toolTip);
-        
+    React.useEffect(() => {
+        if(hasHover){
+            const toolTip = toolTipRef.current;
+
+            toolTip.style.top = `${mouseInfos.y - toolTip.clientHeight}px`; 
+            toolTip.style.left = `${mouseInfos.x + 20}px`;
+            toolTip.style.transform = 'translate(50%, -5px)';
+        }
+    }, [hasHover, isHovering, mouseInfos]);
+
+    function onMouseMove(event){
+        if(hasHover && toolTipRef.current) {
+            setMouseInfos({
+                x: event.pageX, 
+                y: event.pageY,
+                height: toolTipRef.current.offsetHeight,
+            });
+        }
     }
 
-    React.useEffect(() => {
-        handleMouseOver();     
-    }, [mousePositionItem]);
-
-    function handleMouseLeave(){
-        
+    function onMouseOver(){  
+        if(hasHover && toolTipRef.current) setIsHovering(true);
     }
 
-    React.useEffect(() => {
-        handleMouseLeave();
-    }, [mouseLeaveItem]);
+    function onMouseLeave(){
+        if(hasHover && toolTipRef.current) setIsHovering(false);
+    }
 
     return (
         <Link 
-            to={item.id} 
-            className='block w-full mb-[10px] relative'
-            onMouseOver={(event) => {
-                hasHover && setMousePositionItem({
-                    x: event.pageX, 
-                    y: event.pageY, 
-                    element: event.currentTarget,
-            });
-            }}
-            onMouseLeave={() => hasHover && setMouseLeaveItem(false)}
+            to={item.id}
+            className={`tooltip block w-full mb-[10px] ${!hasHover ? 'relative' : ''}`}
+            onMouseMove={onMouseMove}
+            onMouseOver={onMouseOver}
+            onMouseLeave={onMouseLeave}
         >
             <img 
                 className={classItem} 
@@ -47,10 +54,12 @@ const MosaicImage = ({item, classItem}) => {
                 alt={item.grapeName} 
             />
             <div 
-                className='font-sans font-semibold text-xl bg-[rgba(64,64,64,0.8)]
-                text-center text-white rounded-md px-5 py-3
+                ref={toolTipRef}
+                className={`h-fit w-fit max-w-[180px] font-sans font-semibold text-xl 
+                ${!hasHover ? '!block' : ''}
+                bg-[rgba(64,64,64,0.8)] text-center text-white rounded-md px-5 py-3
                 shadow-[0_0_2px_6px_rgba(64,64,64,0.8),0_0_0_2px_#C3A576_inset]
-                absolute z-10 bottom-4 left-1/2 -translate-x-1/2'
+                absolute z-10 bottom-4 left-1/2 -translate-x-1/2`}
             >
                 {item.grapeName}
             </div>
@@ -58,4 +67,4 @@ const MosaicImage = ({item, classItem}) => {
     )
 }
 
-export default MosaicImage
+export default MosaicImage;
